ce79ce2094a86e9d29cadbe6d9d3695514326dfa
Ashish Ranjan <ashishranjan2912@gmail.com>
2018-09-28 10:36:22 +0530

    router bug fix

--- client/src/app/app.router.ts
+++ client/src/app/app.router.ts
@@ -1,25 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
-// import { PostsModule } from './posts/posts.module';
 
 const routes: Routes = [
   {
     path: '',
-    // redirectTo: 'posts',
-    children: [
-      { path: '', redirectTo: 'posts', pathMatch: 'full' },
-      { path: 'posts', loadChildren: './posts/posts.module#PostsModule' }
-    ]
+    redirectTo: 'posts',
+    pathMatch: 'full'
+  },
+  {
+    path: 'posts',
+    loadChildren: './posts/posts.module#PostsModule',
+    pathMatch: 'full'
   },
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
   exports: [ RouterModule ],
-  providers: [
-    {provide: APP_BASE_HREF, useFactory : () => location.pathname },
-  ]
+  providers: [{provide: APP_BASE_HREF, useValue : '/' }]
 })
 
 export class AppRoutingModule {

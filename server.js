586e57c7a2d9537f5ef74c164c7de2f16c425ef7
Ashish Ranjan <ashishranjan2912@gmail.com>
2018-09-28 08:29:34 +0530

    created angular app, linked with express

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var bodyParser = require('body-parser');
 var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/client/dist/client'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(cookieParser());
